15102ec72c3050a1dbb2d0e2ad27bec5e5c49d03
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-07-25 14:05:10 +0300

    готово

---
+++ task/Read_A.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <io.h>
+#include "Read_A.h"
+using std::cout;
+using std::endl;
+
+// Читает текстовый файл и выводит.
+void Read_A::v_Display(const char* path)
+{
+	system("cls");
+	FILE* f_read = nullptr;
+	fopen_s(&f_read, path, "r");
+	if (f_read == nullptr)
+	{
+		perror("Error opening");
+		return;
+	}
+	const int lenght = _filelength(_fileno(f_read));
+	char* arr = new char[lenght] {'\0'};
+	char buffer[300];
+
+	while (fgets(buffer, lenght, f_read))
+		strcat_s(arr, lenght, buffer);
+	fclose(f_read);
+
+	system("cls");
+	cout << arr
+		<< endl;
+}
+
+// Читает и выводит содержимое текстового файла в консоль, в виде ASCIIкодов символов.
+void Read_B::v_Display(const char* path)
+{
+	system("cls");
+	FILE* f_read = nullptr;
+	fopen_s(&f_read, path, "r");
+	if (f_read == nullptr)
+	{
+		perror("Error opening");
+		return;
+	}
+	const int lenght = _filelength(_fileno(f_read));
+	char* arr = new char[lenght] {'\0'};
+	char buffer[300];
+
+	while (fgets(buffer, lenght, f_read))
+		strcat_s(arr, lenght, buffer);
+	fclose(f_read);
+
+	system("cls");
+	for (int i = 0; i < lenght; i++)
+	{
+		cout << int(arr[i]);
+		if (int(arr[i] == 10))
+			cout << '\n';
+	}
+	cout << endl;
+}
+
+// Читает и выводит содержимое текстового файла в консоль, в шестнадцатеричном виде.
+void Read_C::v_Display(const char* path)
+{
+	system("cls");
+	FILE* f_read = nullptr;
+	fopen_s(&f_read, path, "r");
+	if (f_read == nullptr)
+	{
+		perror("Error opening");
+		return;
+	}
+	const int lenght = _filelength(_fileno(f_read));
+	char* arr = new char[lenght] {'\0'};
+	char buffer[300];
+
+	while (fgets(buffer, lenght, f_read))
+		strcat_s(arr, lenght, buffer);
+	fclose(f_read);
+
+	system("cls");
+	for (int i = 0; i < lenght; i++)
+	{
+		//cout << int(arr[i]);
+		printf("%x", arr[i]);
+		if (int(arr[i] == 10))
+			cout << '\n';
+	}
+	cout << endl;
+}
---
+++ task/Read_A.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+class Read_A
+{
+public:
+	// Читает и выводит содержимое текстового файла в консоль.
+	virtual void v_Display(const char* path);
+};
+
+class Read_B :public Read_A
+{
+	// Читает и выводит содержимое текстового файла в консоль, в виде ASCIIкодов символов.
+	void v_Display(const char* path) override;
+};
+
+class Read_C :public Read_A
+{
+	// Читает и выводит содержимое текстового файла в консоль, в шестнадцатеричном виде.
+	void v_Display(const char* path) override;
+};
